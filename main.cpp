efcf111bb9b1b208de7a808f510c61d85dc61d0e
tung <183839212+tungcder@users.noreply.github.com>
2025-03-18 03:30:14 +0700

    Tạo class game, vẽ map

--- main.cpp
+++ main.cpp
@@ -1,114 +1,81 @@
 #include <iostream>
 #include <SDL.h>
-#include <SDL_image.h>
 
 using namespace std;
 
 const int SCREEN_WIDTH = 800;
 const int SCREEN_HEIGHT = 600;
-const char* WINDOW_TITLE = "Hello World!";
+const int TILE_SIZE = 40;
+const int MAP_WIDTH = SCREEN_WIDTH / TILE_SIZE;
+const int MAP_HEIGHT = SCREEN_HEIGHT / TILE_SIZE; // Sửa lỗi đánh máy MAP_HEGHT -> MAP_HEIGHT
 
-void logErrorAndExit(const char* msg, const char* error)
-{
-    SDL_LogMessage(SDL_LOG_CATEGORY_APPLICATION, SDL_LOG_PRIORITY_ERROR, "%s: %s", msg, error);
-    SDL_Quit();
+class Game {
+public:
+    SDL_Window* window;
+    SDL_Renderer* renderer;
+    bool running;
+
+    Game();
+    void render();
+    void run();
+    ~Game();
+};
+
+// Constructor
+Game::Game() {
+    running = true;
+    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+        cerr << "SDL could not initialize! SDL_Error: " << SDL_GetError() << endl;
+        running = false;
+    }
+    window = SDL_CreateWindow("Battle City", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, SCREEN_WIDTH,
+                              SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+    if (!window) {
+        cerr << "Window could not be created! SDL_Error: " << SDL_GetError() << endl;
+        running = false;
+    }
+    renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+    if (!renderer) {
+        cerr << "Renderer could not be created! SDL_Error: " << SDL_GetError() << endl;
+        running = false;
+    }
 }
 
-SDL_Window* initSDL(int SCREEN_WIDTH, int SCREEN_HEIGHT, const char* WINDOW_TITLE)
-{
-    if (SDL_Init(SDL_INIT_EVERYTHING) != 0)
-        logErrorAndExit("SDL_Init", SDL_GetError());
+// Render function
+void Game::render() {
+    SDL_SetRenderDrawColor(renderer, 128, 128, 128, 255);
+    SDL_RenderClear(renderer);
 
-    SDL_Window* window = SDL_CreateWindow(WINDOW_TITLE, SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
-    //full screen
-    //window = SDL_CreateWindow(WINDOW_TITLE, SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_FULLSCREEN_DESKTOP);
-    if (window == nullptr) logErrorAndExit("CreateWindow", SDL_GetError());
-
-    if (!IMG_Init(IMG_INIT_PNG | IMG_INIT_JPG))
-        logErrorAndExit( "SDL_image error:", IMG_GetError());
-
-    return window;
+    SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+    for (int i = 1; i < MAP_HEIGHT - 1; ++i) {
+        for (int j = 1; j < MAP_WIDTH - 1; ++j) {
+            SDL_Rect tile = { j * TILE_SIZE, i * TILE_SIZE, TILE_SIZE, TILE_SIZE };
+            SDL_RenderFillRect(renderer, &tile); // Vẽ ô vuông lên màn hình
+        }
+    }
+    SDL_RenderPresent(renderer);
 }
 
-SDL_Renderer* createRenderer(SDL_Window* window)
-{
-    SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED |
-                                              SDL_RENDERER_PRESENTVSYNC);
-    //Khi chạy trong máy ảo (ví dụ phòng máy ở trường)
-    //renderer = SDL_CreateSoftwareRenderer(SDL_GetWindowSurface(window));
-
-    if (renderer == nullptr) logErrorAndExit("CreateRenderer", SDL_GetError());
-
-    SDL_SetHint(SDL_HINT_RENDER_SCALE_QUALITY, "linear");
-    SDL_RenderSetLogicalSize(renderer, SCREEN_WIDTH, SCREEN_HEIGHT);
-
-    return renderer;
+// Run loop
+void Game::run() {
+    while (running) {
+        render();
+        SDL_Delay(16);
+    }
 }
 
-void quitSDL(SDL_Window* window, SDL_Renderer* renderer)
-{
-    IMG_Quit();
-
+// Destructor
+Game::~Game() {
     SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(window);
     SDL_Quit();
 }
 
-void waitUntilKeyPressed()
-{
-    SDL_Event e;
-    while (true) {
-        if ( SDL_PollEvent(&e) != 0 &&
-             (e.type == SDL_KEYDOWN || e.type == SDL_QUIT) )
-            return;
-        SDL_Delay(100);
+// Main function
+int main(int argc, char* argv[]) {
+    Game game;
+    if (game.running) {
+        game.run();
     }
-}
-
-void renderTexture(SDL_Texture *texture, int x, int y, SDL_Renderer* renderer)
-{
-	SDL_Rect dest;
-
-	dest.x = x;
-	dest.y = y;
-	SDL_QueryTexture(texture, NULL, NULL, &dest.w, &dest.h);
-
-	SDL_RenderCopy(renderer, texture, NULL, &dest);
-}
-
-SDL_Texture *loadTexture(const char *filename, SDL_Renderer* renderer)
-{
-	SDL_LogMessage(SDL_LOG_CATEGORY_APPLICATION, SDL_LOG_PRIORITY_INFO, "Loading %s", filename);
-
-	SDL_Texture *texture = IMG_LoadTexture(renderer, filename);
-	if (texture == NULL)
-        SDL_LogMessage(SDL_LOG_CATEGORY_APPLICATION, SDL_LOG_PRIORITY_ERROR, "Load texture %s", IMG_GetError());
-
-	return texture;
-}
-
-int main(int argc, char *argv[])
-{
-    SDL_Window* window = initSDL(SCREEN_WIDTH, SCREEN_HEIGHT, WINDOW_TITLE);
-    SDL_Renderer* renderer = createRenderer(window);
-
-    SDL_Texture* background = loadTexture("bikiniBottom.jpg", renderer);
-    SDL_RenderCopy( renderer, background, NULL, NULL);
-
-    SDL_RenderPresent( renderer );
-    waitUntilKeyPressed();
-
-    SDL_Texture* spongeBob = loadTexture("Spongebob.png", renderer);
-    renderTexture(spongeBob, 200, 200, renderer);
-
-    SDL_RenderPresent( renderer );
-    waitUntilKeyPressed();
-
-    SDL_DestroyTexture( spongeBob );
-    spongeBob = NULL;
-    SDL_DestroyTexture( background );
-    background = NULL;
-
-    quitSDL(window, renderer);
     return 0;
 }
